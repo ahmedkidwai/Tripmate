ed64fe74a024b2eea2d4f9dc1bef38f46bfdcbeb
williamconnorbean <beanw@myumanitoba.ca>
2020-02-09 16:05:21 -0600

    TM-fix_redux_dev_tools_error
    - adds the redux dev tools window object if the chrome extensions are
    installed in browser
    - passes an empty function otherwise since `compose` is expecting
    functions as parameters

--- web-application/src/index.js
+++ web-application/src/index.js
@@ -12,7 +12,8 @@ const store = createStore(
     rootReducer,
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ?
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : emptyFunc => emptyFunc
     )
 );
 
