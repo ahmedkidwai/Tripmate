0660ad775ddab4aa73075ddf000b7d959bfcb115
Connor Bean <beanw@myumanitoba.ca>
2020-03-02 16:46:12 -0600

    Allow mobile app to talk to production server
    - Added function to determine api url based on the `__DEV__` react
      native variable
    - Updated user action
    - Updated tests

--- mobile/src/utils/globalVars.js
+++ mobile/src/utils/globalVars.js
@@ -1,4 +1,3 @@
-import {Platform} from 'react-native';
+import {Environment} from '../../src/utils/environment';
 
-export const url =
-  Platform.OS === 'ios' ? 'http://localhost:5000' : 'http://10.0.2.2:5000';
+export const url = Environment.BASE_URL;
