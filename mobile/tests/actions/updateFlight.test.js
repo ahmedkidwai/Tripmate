31826ef1c446e2ac4cbf61e3a52f4278a8656901
Renan Turra <renan.turra@gmail.com>
2020-03-26 12:02:49 -0500

    TM-004--actions-and-reducers-for-updateTicket-and-updateFlight--mobile
    fix lint errors

--- mobile/tests/actions/updateFlight.test.js
+++ mobile/tests/actions/updateFlight.test.js
@@ -4,6 +4,7 @@ import MockAdapter from 'axios-mock-adapter';
 import axios from 'axios';
 import expect from 'expect';
 import * as actions from '../../src/actions/updateFlight';
+import {url} from '../../src/utils/globalVars';
 
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
@@ -26,26 +27,27 @@ describe('update flight actions', () => {
 
   it('creates UPDATE_FLIGHT_SUCCESS when delete flight has been done', () => {
     mockAxios
-      .onPost(`/flight/update/${flight._id}`)
+      .onPost(`${url}/flight/update/${flight._id}`)
       .reply(200, [{data: 'Flight updated.'}]);
-    return store.dispatch(actions.updateFlight(flight._id, 'Test update')).then(() => {
-      const expectedActions = [
-        {type: actions.UPDATE_FLIGHT_BEGIN},
-        {
-          type: actions.UPDATE_FLIGHT_SUCCESS,
-          payload: {
-            successMessage: [{data: 'Flight updated.'}],
+    return store
+      .dispatch(actions.updateFlight(flight._id, 'Test update'))
+      .then(() => {
+        const expectedActions = [
+          {type: actions.UPDATE_FLIGHT_BEGIN},
+          {
+            type: actions.UPDATE_FLIGHT_SUCCESS,
+            payload: {
+              successMessage: [{data: 'Flight updated.'}],
+            },
           },
-        },
-      ];
-      expect(store.getActions()).toEqual(expectedActions);
-    });
+        ];
+        expect(store.getActions()).toEqual(expectedActions);
+      });
   });
-  
+
   it('creates UPDATE_FLIGHT_FAILURE when updating flight has failed', () => {
-    const testFlightID = 12345;
-    mockAxios.onPost(`/flight/update/${testFlightID}`).reply(500);
-    
+    mockAxios.onPost(`${url}/flight/update/${flight._id}`).reply(500);
+
     return store.dispatch(actions.updateFlight()).then(() => {
       const storeActions = store.getActions();
       expect(storeActions[0]).toHaveProperty('type', 'UPDATE_FLIGHT_BEGIN');
--- mobile/tests/actions/updateTicket.test.js
+++ mobile/tests/actions/updateTicket.test.js
@@ -4,6 +4,7 @@ import MockAdapter from 'axios-mock-adapter';
 import axios from 'axios';
 import expect from 'expect';
 import * as actions from '../../src/actions/updateTicket';
+import {url} from '../../src/utils/globalVars';
 
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
@@ -33,26 +34,27 @@ describe('update ticket actions', () => {
 
   it('creates UPDATE_TICKET_SUCCESS when delete ticket has been done', () => {
     mockAxios
-      .onPost(`/ticket/update/${ticket._id}`)
+      .onPost(`${url}/ticket/update/${ticket._id}`)
       .reply(200, [{data: 'Ticket updated.'}]);
-    return store.dispatch(actions.updateTicket(ticket._id, 'Test update')).then(() => {
-      const expectedActions = [
-        {type: actions.UPDATE_TICKET_BEGIN},
-        {
-          type: actions.UPDATE_TICKET_SUCCESS,
-          payload: {
-            successMessage: [{data: 'Ticket updated.'}],
+    return store
+      .dispatch(actions.updateTicket(ticket._id, 'Test update'))
+      .then(() => {
+        const expectedActions = [
+          {type: actions.UPDATE_TICKET_BEGIN},
+          {
+            type: actions.UPDATE_TICKET_SUCCESS,
+            payload: {
+              successMessage: [{data: 'Ticket updated.'}],
+            },
           },
-        },
-      ];
-      expect(store.getActions()).toEqual(expectedActions);
-    });
+        ];
+        expect(store.getActions()).toEqual(expectedActions);
+      });
   });
-  
+
   it('creates UPDATE_TICKET_FAILURE when updating ticket has failed', () => {
-    const testTicketID = 12345;
-    mockAxios.onPost(`/ticket/update/${testTicketID}`).reply(500);
-    
+    mockAxios.onPost(`${url}/ticket/update/${ticket._id}`).reply(500);
+
     return store.dispatch(actions.updateTicket()).then(() => {
       const storeActions = store.getActions();
       expect(storeActions[0]).toHaveProperty('type', 'UPDATE_TICKET_BEGIN');
